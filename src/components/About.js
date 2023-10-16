c5747698a0bdb082a3dca45ddaffb5149af6dcdc
Sainadh <sainadhram1123@gmail.com>
2023-10-16 19:30:23 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+// AboutMe.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+      Hi there, I’m a 3rd year B. Tech student, equipped with a wide array of skills. 
+      I also thrive as a great communicator and a team player, excited to collaborate 
+      and grow in a challenging environment!
+      
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="mailto:sainadhram1123@gmail.com">sainadhram1123@gmail.com</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/sainadh-reddy-471000226" target="_blank" rel="noopener noreferrer">Linkedin profile</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/iSainadh" target="_blank" rel="noopener noreferrer">github profile</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
